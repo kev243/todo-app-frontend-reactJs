2001e6cf56eec832b260bbb78bdd1baa74c99f65
nimikevin <97411659+nimikevin@users.noreply.github.com>
2023-02-05 06:50:40 -0500

    travail sur l'interface avant ajout de la BD

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "./App.css";
 import TaskForm from "./components/taskForm/TaskForm";
-import TaskList from "./components/taskList/TaskList";
 
 const App = () => {
   return (
     <div className="todo-container">
-      <div className="todo-content">
-        <div className="header">
-          <h1>Task Manager</h1>
+      <div className="header">
+        <h1>Task Manager</h1>
+      </div>
+      <TaskForm />
+      <div className="list-container">
+        <div className="task-list">
+          <input type={"checkbox"} />
+          <label>Dormir avec la climatisation</label>
         </div>
-        <TaskForm />
-        <TaskList />
+
+        <div className="task-list">
+          <input type={"checkbox"} />
+          <label>Dormir avec la climatisation</label>
+        </div>
+        <div className="task-list">
+          <input type={"checkbox"} />
+          <label>Dormir avec la climatisation</label>
+        </div>
       </div>
     </div>
   );
